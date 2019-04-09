67e41736335fffdbb0c50cc3f9070b254138caef
Khaled Khalifeh <khaled.khalifeh@gmail.com>
2019-04-09 12:40:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+
+function heyJude() {
+  let string = "Na na na na na na na, na na na na, hey Jude."
+  return string.repeat(16)
+}
+
+let prohibitedLanguage = (string) => {
+  let bannedWords = /candycorn|brusselssprouts/;
+  return string.search(bannedWords);
+}
