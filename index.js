6fabb92e34f1cb91635943eea62d85d5f966c672
github username <github email address>
2019-04-11 14:45:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here
+
+function heyJude(verse){
+  return verse.repeat(16)
+
+}
+
+let prohibitedLanguage = (string) => {
+  let bannedWords = /candycorn|brusselssprouts/;
+  return string.search(bannedWords);
+}
