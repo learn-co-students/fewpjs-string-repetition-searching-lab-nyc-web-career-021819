d26ff06e6d0f71695b4e36ead5169acff27cad01
adamjgordon28 <adamjgordon24@gmail.com>
2019-04-11 16:21:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution here
+function heyJude() {
+  let string = "Na na na na na na na, na na na na, hey Jude."
+
+  return (new Array(17)).join(string);
+}
+
+
+function prohibitedLanguage(string) {
+  return string.search(/candycorn|brusselssprouts/)
+}
