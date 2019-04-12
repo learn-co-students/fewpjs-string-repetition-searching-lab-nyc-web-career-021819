4b6b8219639e5f16fe553d9e51b04af5768ae426
aort93 <alonsojortega@gmail.com>
2019-04-12 10:02:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+
+const heyJude = (verse) => {
+  return verse.repeat(27);
+}
+
+const prohibitedLanguage = (name) => {
+  if (name.indexOf('candycorn') > -1 ){
+    return name.indexOf('candycorn')
+  } else if (name.indexOf('brusselssprouts') > -1) {
+    return name.indexOf('brusselssprouts')
+  } else {
+    return -1
+  }
+}
