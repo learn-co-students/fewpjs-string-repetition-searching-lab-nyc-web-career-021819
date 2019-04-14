5364008361cab3b9059daaeeeaaea9026ea635c8
richietam <richardtam.ny@gmail.com>
2019-04-13 22:10:06 -0400

    Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+
+function heyJude (verse) {
+  return verse.repeat(16)
+}
+
+function prohibitedLanguage (string) {
+  prohibitedLanguage = /candycorn|brusselssprouts/
+  return string.search(prohibitedLanguage)
+}
